06905dcb42edb31844c3235d4e5bbabcb0aa510f
Long Nguyen <nlhlong@protonmail.com>
2021-01-08 15:33:42 +0100

    minor

---
+++ game.js
@@ -0,0 +1,28 @@
+const { getDeck, shuffle, countCards, playOneRound } = require('./src/actions');
+
+const deck = shuffle(getDeck());
+const player1 = {
+  name: 'Player 1',
+  drawPile: deck.filter((v, i) => i % 2 !== 0),
+  discardPile: []
+};
+const player2 = {
+  name: 'Player 2',
+  drawPile: deck.filter((v, i) => i % 2 === 0),
+  discardPile: []
+};
+
+console.log('Initiate a new deck', deck);
+console.log(`${player1.name}'s draw pile:`, player1.drawPile);
+console.log(`${player2.name}'s draw pile:`, player2.drawPile);
+
+// Play until one player has 0 card left
+while (countCards(player1) * countCards(player2) !== 0) {
+  const roundWinner = (
+    playOneRound(player1, player2, true) ? player1.name : player2.name
+  );
+  console.log(`${roundWinner} wins this round.\n`);
+}
+
+const winner = countCards(player1) === 0 ? player2.name : player1.name;
+console.log(`${winner} wins the game!!`);
